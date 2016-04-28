1f87766276b2aa4f561bd8e39a5f54844f68cc6a
Jokke <jonnejokke2@gmail.com>
2016-04-28 21:51:14 +0300

    Delete Utilities.cs

--- Twitchiedll/Utils/Utilities.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Twitchiedll.Utils
-{
-    internal class Utilities
-    {
-        internal static bool ConvertToBoolean(string Str) => Str.Equals("1");
-
-        internal static bool IsChannelMessage(string Buffer) => Buffer.Contains("PRIVMSG");
-    }
-}
